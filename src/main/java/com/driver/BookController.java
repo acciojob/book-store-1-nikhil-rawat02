451054c74428863637d55113aefc4cc6590cb677
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-02-20 11:27:21 +0530

    1 solution

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -1,18 +1,13 @@
 package com.driver;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -45,30 +40,79 @@ public class BookController {
     // post request /create-book
     // pass book as request body
     @PostMapping("/create-book")
-    public ResponseEntity<Book> createBook(@RequestBody Book book){
+    public ResponseEntity<Book> createBook(@RequestBody() Book book){
         // Your code goes here.
+        book.setId(bookList.size());
+        this.bookList.add(book);
+        System.out.println(bookList.size());
         return new ResponseEntity<>(book, HttpStatus.CREATED);
     }
 
     // get request /get-book-by-id/{id}
     // pass id as path variable
-    // getBookById()
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable("id") int iD){
+        for(Book book : bookList){
+            if (book.getId() == iD)return new ResponseEntity<>(book, HttpStatus.CREATED);
+        }
+        return null;
+     }
 
     // delete request /delete-book-by-id/{id}
     // pass id as path variable
-    // deleteBookById()
+    @DeleteMapping("/delete-book-by-id/{id}")
+     public ResponseEntity<String> deleteBookById(@PathVariable("id") int iD){
+        int idx = -1;
+        for(int i =0; i < bookList.size(); i++){
+            if(bookList.get(i).getId() == iD){
+                idx = i;
+            }
+        }
+        if(idx != -1){
+            bookList.remove(idx);
+            return new ResponseEntity<>(iD + "has been deleted",HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("ID not found", HttpStatus.CREATED);
+     }
 
     // get request /get-all-books
-    // getAllBooks()
+    @GetMapping("/get-all-books")
+     public ResponseEntity<Map<String,Book>> getAllBooks(){
+        Map <String, Book> map = new HashMap<>();
+            for (Book book : bookList) {
+            map.put(Integer.toString(book.getId()), book);
+        }
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+     }
 
     // delete request /delete-all-books
-    // deleteAllBooks()
+    @DeleteMapping("/delete-all-books")
+     public String deleteAllBooks(){
+        bookList.clear();
+        return "All books in bookList has been deleted" + bookList.size();
+     }
 
     // get request /get-books-by-author
     // pass author name as request param
-    // getBooksByAuthor()
+    @GetMapping("/get-books-by-author")
+     public ResponseEntity<Book> getBooksByAuthor(@RequestParam("author") String author){
+        for(Book book : bookList){
+            if(book.getAuthor().equals(author)){
+                return new ResponseEntity<>(book, HttpStatus.CREATED);
+            }
+        }
+        return null;
+     }
 
     // get request /get-books-by-genre
     // pass genre name as request param
-    // getBooksByGenre()
+    @GetMapping("/get-books-by-genre")
+     public ResponseEntity<Book> getBooksByGenre(@RequestParam("genre") String genre){
+        for(Book book : bookList){
+            if (book.getGenre().equals(genre)){
+                return new ResponseEntity<>(book, HttpStatus.CREATED);
+            }
+        }
+        return null;
+     }
 }
